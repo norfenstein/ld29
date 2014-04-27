29db2f079aa509ab4110313d42b27a648faaa0ea
Norfenstein <norfenstein@users.noreply.github.com>
2014-04-27 16:09:01 -0400

    diving!

--- core/src/norfenstein/ld29/GameScreen.java
+++ core/src/norfenstein/ld29/GameScreen.java
@@ -15,9 +15,11 @@ import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.EdgeShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Joint;
 import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.joints.RopeJointDef;
 import norfenstein.ld29.RenderableBody.FillType;
 import norfenstein.util.entities.EntityStore.Entity;
 import norfenstein.util.entities.EntityStore;
@@ -28,10 +30,11 @@ public class GameScreen extends ViewportScreen {
 	private EntityStore entityStore;
 	private ShapeRenderSystem shapeRenderSystem;
 
-	private final float UNITS_PER_SCREEN = 30f;
+	private final float UNITS_PER_SCREEN = 40f;
 	private final float WATER_DEPTH = 8f;
-	private final float MAX_FLAP_STRENGTH = 40f;
+	private final float MAX_FLAP_IMPULSE = 40f;
 	private final float FLAP_REGEN_TIME = 0.7f;
+	private final float DIVE_FORCE = 100f;
 	private final float GRAVITY = 15f;
 
 	private final short COLLISION_NONE  = 0;
@@ -40,9 +43,11 @@ public class GameScreen extends ViewportScreen {
 	private final short COLLISION_FISH  = 1 << 2;
 	private final short COLLISION_WATER = 1 << 3;
 
-	private float flapStrength;
+	private Body anchorBody; //static body without fixtures for connecting joints to the world
 	private Body birdBody;
 	private Body waterBody;
+	private float flapImpulse;
+	private Joint divingJoint;
 
 	public void create() {
 		initializeViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), FixedAxis.HORIZONTAL, UNITS_PER_SCREEN);
@@ -56,7 +61,10 @@ public class GameScreen extends ViewportScreen {
 
 		entityStore.addListener(shapeRenderSystem);
 
-		flapStrength = MAX_FLAP_STRENGTH;
+		divingJoint = null;
+		flapImpulse = MAX_FLAP_IMPULSE;
+
+		addAnchor();
 		addBird();
 		addWalls();
 		addWater();
@@ -70,9 +78,15 @@ public class GameScreen extends ViewportScreen {
 	@Override public void step(float delta) {
 		world.step(delta, 6, 2);
 
-		if (flapStrength < MAX_FLAP_STRENGTH) {
-			flapStrength += MAX_FLAP_STRENGTH * delta / FLAP_REGEN_TIME;
-			if (flapStrength > MAX_FLAP_STRENGTH) flapStrength = MAX_FLAP_STRENGTH;
+		if (flapImpulse < MAX_FLAP_IMPULSE) {
+			flapImpulse += MAX_FLAP_IMPULSE * delta / FLAP_REGEN_TIME;
+			if (flapImpulse > MAX_FLAP_IMPULSE) flapImpulse = MAX_FLAP_IMPULSE;
+		}
+
+		if (divingJoint != null) {
+			if (birdBody.getPosition().y >= anchorBody.getWorldPoint(divingJoint.getAnchorA()).y) {
+				destroyDivingJoint();
+			}
 		}
 	}
 
@@ -85,6 +99,12 @@ public class GameScreen extends ViewportScreen {
 		super.resize(width, height);
 	}
 
+	@Override public void processInput(float delta) {
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			birdBody.applyForce(new Vector2(0, -DIVE_FORCE), birdBody.getWorldCenter(), true);
+		}
+	}
+
 	@Override public boolean keyDown(int keycode) {
 		switch (keycode) {
 			case Input.Keys.ESCAPE:
@@ -95,12 +115,51 @@ public class GameScreen extends ViewportScreen {
 
 			case Input.Keys.UP:
 			case Input.Keys.SPACE:
-				birdBody.applyLinearImpulse(new Vector2(0, flapStrength), birdBody.getWorldCenter(), true);
-				flapStrength = 0f;
+				if (divingJoint != null) destroyDivingJoint();
+				birdBody.applyLinearImpulse(new Vector2(0, flapImpulse), birdBody.getWorldCenter(), true);
+				flapImpulse = 0f;
+				return true;
+
+			case Input.Keys.LEFT:
+				if (divingJoint != null) destroyDivingJoint();
+				birdBody.applyLinearImpulse(new Vector2(0, flapImpulse).rotate(30), birdBody.getWorldCenter(), true);
+				flapImpulse = 0f;
+				return true;
+
+			case Input.Keys.RIGHT:
+				if (divingJoint != null) destroyDivingJoint();
+				birdBody.applyLinearImpulse(new Vector2(0, flapImpulse).rotate(-30), birdBody.getWorldCenter(), true);
+				flapImpulse = 0f;
+				return true;
+
+			case Input.Keys.DOWN:
+				if (divingJoint != null) destroyDivingJoint();
+				flapImpulse = 0f;
 				return true;
 		}
 
 		return false;
+	}
+
+	@Override public boolean keyUp(int keycode) {
+		switch (keycode) {
+			case Input.Keys.DOWN:
+				addDivingJoint();
+				return true;
+		}
+
+		return false;
+	}
+
+	private void addAnchor() {
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyType.StaticBody;
+		bodyDef.position.x = 0;
+		bodyDef.position.y = 0;
+
+		Body body = world.createBody(bodyDef);
+
+		anchorBody = body;
 	}
 
 	private void addBird() {
@@ -135,40 +194,45 @@ public class GameScreen extends ViewportScreen {
 
 		Entity entity = entityStore.new Entity(physicsBody, renderableBody);
 		body.setUserData(entity);
-		//body.applyLinearImpulse(new Vector2(0, flapStrength).rotate(-30f), body.getWorldCenter(), true);
 
 		birdBody = body;
 	}
 
 	private void addWalls() {
-		addWall(
+		//Bottom
+		addWall(0.0f,
 			pixelsToUnits(-getViewport().getViewportWidth() / 2f),
 			pixelsToUnits(-getViewport().getViewportHeight() / 2f + 1),
 			pixelsToUnits(getViewport().getViewportWidth() / 2f),
 			pixelsToUnits(-getViewport().getViewportHeight() / 2f + 1)
 		);
-		addWall(
+
+		//Top
+		addWall(0.0f,
 			pixelsToUnits(-getViewport().getViewportWidth() / 2f),
 			pixelsToUnits(getViewport().getViewportHeight() / 2f - 1),
 			pixelsToUnits(getViewport().getViewportWidth() / 2f),
 			pixelsToUnits(getViewport().getViewportHeight() / 2f - 1)
 		);
-		addWall(
+
+		//Left
+		addWall(1.0f,
 			pixelsToUnits(-getViewport().getViewportWidth() / 2f + 1),
 			pixelsToUnits(-getViewport().getViewportHeight() / 2f),
 			pixelsToUnits(-getViewport().getViewportWidth() / 2f + 1),
 			pixelsToUnits(getViewport().getViewportHeight() / 2f)
 		);
-		addWall(
+
+		//Right
+		addWall(1.0f,
 			pixelsToUnits(getViewport().getViewportWidth() / 2f - 1),
 			pixelsToUnits(-getViewport().getViewportHeight() / 2f),
 			pixelsToUnits(getViewport().getViewportWidth() / 2f - 1),
 			pixelsToUnits(getViewport().getViewportHeight() / 2f)
 		);
 	}
-	private void addWall(float x1, float y1, float x2, float y2) {
+	private void addWall(float restitution, float x1, float y1, float x2, float y2) {
 		EdgeShape shape = new EdgeShape();
-		System.out.println("width: " + getViewport().getViewportWidth() + ", height: " + getViewport().getViewportHeight());
 		shape.set(x1, y1, x2, y2);
 
 		FixtureDef fixtureDef = new FixtureDef();
@@ -234,13 +298,60 @@ public class GameScreen extends ViewportScreen {
 		waterBody = body;
 	}
 
+	private void addDivingJoint() {
+		if (birdBody.getLinearVelocity().y >= 0) return;
+
+		float fieldWidth = pixelsToUnits(getViewport().getViewportWidth());
+		float fieldEdgeX = fieldWidth / 2;
+		float fieldHeight = pixelsToUnits(getViewport().getViewportHeight());
+		float fieldEdgeY = fieldHeight / 2;
+		float bufferSize = 1f; //a little more than bird's radius, to avoid scraping the edge on the upswing
+		float birdX = birdBody.getPosition().x;
+		float birdY = birdBody.getPosition().y;
+		float targetY = waterBody.getPosition().y;
+		if (targetY > birdY) targetY = -fieldEdgeY + bufferSize;
+		float idealRadius = birdY - targetY;
+
+		float anchorX = 0f;
+		if (birdX <= 0) { //going to the right
+			if (birdX + idealRadius * 2 < fieldEdgeX - bufferSize) {
+				anchorX = birdX + idealRadius;
+			} else {
+				anchorX = birdX + (fieldEdgeX - birdX) / 2 - bufferSize;
+			}
+		} else if (birdX > 0) { //going to the left
+			if (birdX - idealRadius * 2 > -fieldEdgeX + bufferSize) {
+				anchorX = birdX - idealRadius;
+			} else {
+				anchorX = birdX - (fieldEdgeX + birdX) / 2 + bufferSize;
+			}
+		} else {
+			return;
+		}
+		Vector2 anchorPoint = new Vector2(anchorX, birdBody.getPosition().y);
+
+		RopeJointDef ropeJointDef = new RopeJointDef();
+		ropeJointDef.bodyA = anchorBody;
+		ropeJointDef.bodyB = birdBody;
+		ropeJointDef.localAnchorA.set(anchorBody.getLocalPoint(anchorPoint));
+		ropeJointDef.localAnchorB.set(Vector2.Zero);
+		ropeJointDef.maxLength = birdBody.getPosition().dst(anchorPoint);
+
+		if (divingJoint != null) destroyDivingJoint();
+		divingJoint = world.createJoint(ropeJointDef);
+	}
+
+	private void destroyDivingJoint() {
+		world.destroyJoint(divingJoint);
+		divingJoint = null;
+	}
+
 	private class CollisionHandler implements ContactListener {
 		@Override public void preSolve(Contact contact, Manifold oldManifold) { }
 
 		@Override public void postSolve(Contact contact, ContactImpulse impulse) { }
 
 		@Override public void beginContact(Contact contact) {
-			System.out.println("beginContact");
 			Body bodyA = contact.getFixtureA().getBody();
 			Body bodyB = contact.getFixtureB().getBody();
 
@@ -249,7 +360,6 @@ public class GameScreen extends ViewportScreen {
 		}
 
 		@Override public void endContact(Contact contact) {
-			System.out.println("endContact");
 			Body bodyA = contact.getFixtureA().getBody();
 			Body bodyB = contact.getFixtureB().getBody();
 
